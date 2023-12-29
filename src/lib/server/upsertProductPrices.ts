ec29b351e2eecfbfcad5e193184b2e509b668717
whiteHawk <freeidlll6@gmail.com>
2023-12-29 22:39:22 +0530

    added stripe prices automatic update into db

---
+++ src/lib/server/upsertProductPrices.ts
@@ -0,0 +1,32 @@
+import type Stripe from 'stripe';
+import { prisma } from './db';
+
+export const upsertProductPrice = async (price: Stripe.Price) => {
+	await prisma.allSubscriptionPrices.upsert({
+		create: {
+			id: price.id,
+			product: { connect: { id: typeof price.product === 'string' ? price.product : '' } },
+			active: price.active,
+			description: price.nickname,
+			metadata: price.metadata,
+			currency: price.currency,
+			type: price.type,
+			unit_amount: price.unit_amount,
+			interval: price.recurring?.interval,
+			interval_count: price.recurring?.interval_count
+		},
+		update: {
+			id: price.id,
+			product: { connect: { id: typeof price.product === 'string' ? price.product : '' } },
+			active: price.active,
+			description: price.nickname,
+			metadata: price.metadata,
+			currency: price.currency,
+			type: price.type,
+			unit_amount: price.unit_amount,
+			interval: price.recurring?.interval,
+			interval_count: price.recurring?.interval_count
+		},
+		where: { id: price.id }
+	});
+};
--- src/routes/api/webhooks/stripe/+server.ts
+++ src/routes/api/webhooks/stripe/+server.ts
@@ -3,6 +3,7 @@ import { stripe } from '$lib/server/stripe';
 import { json, type RequestHandler } from '@sveltejs/kit';
 import type Stripe from 'stripe';
 import { upsertProductRecord } from '$lib/server/upsertProductsRecord';
+import { upsertProductPrice } from '$lib/server/upsertProductPrices';
 
 const webhookSecret = 'whsec_f6c95e08203bcb2457a409fdbae660137ffc9b06f3ca58ed47153e721065f2c1';
 export const POST: RequestHandler = async ({ request }) => {
@@ -16,13 +17,15 @@ export const POST: RequestHandler = async ({ request }) => {
 			case 'product.updated':
 				await upsertProductRecord(event.data.object as Stripe.Product);
 				break;
+			case 'price.created':
+			case 'price.updated':
+				upsertProductPrice(event.data.object as Stripe.Price);
+				break;
 			default:
 				console.log('We cannot handle that request');
 		}
 	} catch (error: unknown) {
-		const message = `webhook error: ${
-			error instanceof Error ? error.message : ''
-		},${webhookSecret},${payload},${signature}`;
+		const message = `webhook error: ${error instanceof Error ? error.message : ''}`;
 		return new Response(message, { status: 401 });
 	}
 
